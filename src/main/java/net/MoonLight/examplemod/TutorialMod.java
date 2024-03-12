7854a06eefc98ecfc3fe1c58f910752ef4ae37c6
JimCratch <truborain@gmail.com>
2024-03-12 16:26:54 +0000

    Nothing

--- src/main/java/net/MoonLight/examplemod/TutorialMod.java
+++ src/main/java/net/MoonLight/examplemod/TutorialMod.java
@@ -44,6 +44,7 @@ public class TutorialMod {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    // Test
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
