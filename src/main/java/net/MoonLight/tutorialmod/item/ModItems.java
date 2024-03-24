d699a68967f2cd690f591bf70a574924c891eaf4
JimCratch <truborain@gmail.com>
2024-03-24 18:20:06 +0000

    Nothing

--- src/main/java/net/MoonLight/tutorialmod/item/ModItems.java
+++ src/main/java/net/MoonLight/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.MoonLight.tutorialmod.item;
 
 import net.MoonLight.tutorialmod.TutorialMod;
 
+import net.MoonLight.tutorialmod.item.custom.MetalDetectorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,6 +18,9 @@ public class ModItems {
 
     public static final RegistryObject<Item>  uselessitem = ITEMS.register("uselessitem",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item>  METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
 
     public static void register(IEventBus eventBus) {
 
---
+++ src/main/java/net/MoonLight/tutorialmod/modcreativemodetabs/ModCreativeModTabs.java
@@ -0,0 +1,35 @@
+package net.MoonLight.tutorialmod.modcreativemodetabs;
+
+import net.MoonLight.tutorialmod.TutorialMod;
+import net.MoonLight.tutorialmod.block.ModBlocks;
+import net.MoonLight.tutorialmod.item.ModItems;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import javax.swing.*;
+
+public class ModCreativeModTabs {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.uselessitem.get()))
+                    .title(Component.translatable("creativetab.tutorial_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.uselessitem.get());
+                        pOutput.accept(ModBlocks.USELESS_BLOCK.get());
+                        pOutput.accept(ModBlocks.UGLY_BLOCK.get());
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
